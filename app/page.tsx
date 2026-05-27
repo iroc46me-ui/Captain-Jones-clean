5fb816cfedd7d27a56b79d321380cafe3f56e195
iroc46me-ui <iroc46me@gmail.com>
2026-05-27 10:21:14 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -114,10 +114,10 @@ function Hero() {
 
 <motion.div initial={{ opacity: 0, y: 22 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.7 }}>
   <div className="mt-[30.5rem] flex justify-center gap-4">
-    <a href="#join" className="rounded-md border border-transparent px-24 py-9 text-sm font-bold text-white hover:bg-amber-300/10">
+    <a href="#join" className="rounded-md border border-transparent px-28 py-7 text-sm font-bold text-white hover:bg-amber-300/10">
       
     </a>
-    <a href="#marketplace" className="rounded-md border border-transparent px-24 py-9 text-sm font-bold text-white hover:bg-amber-300/10">
+    <a href="#marketplace" className="rounded-md border border-transparent px-28 py-7 text-sm font-bold text-white hover:bg-amber-300/10">
       
     </a>
   </div>
