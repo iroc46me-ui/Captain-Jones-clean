a7db82c3120680efb083cd75032ef8cfff1274af
iroc46me-ui <iroc46me@gmail.com>
2026-06-19 12:12:00 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -903,7 +903,7 @@ export default function Home() {
       <HarborFees/>
       <ShippingReturns/>
       <HarborReputation/>
-      <IdwentityAndBuild/>
+      <IdentityAndBuild/>
       <SellerInviteForm/>
       <About/>
       <Footer/>
